fecb90a141a7fde76926c2b9f434b5af2688c6d2
Giousa <65489469@qq.com>
2017-03-21 09:33:54 +0800

    first commit

---
+++ app/src/main/java/com/llew/e/clear/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.llew.e.clear.view.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.llew.e.clear.R;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
